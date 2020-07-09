3fe3d1baed58b118af0ce8ba4910b791b9b41c10
tanay <tanayshah@live.com>
2020-07-09 17:22:06 +0530

    'x'

--- api3.py
+++ api3.py
@@ -2,13 +2,30 @@ from flask import Flask, request, render_template, url_for
 import api1
 app = Flask(__name__)
 
-url = api1.createPayment("getfrompc",30)
+from instamojo_wrapper import Instamojo
+
+app = Flask(__name__)
+#model = pickle.load(open('model.pkl', 'rb'))
+API_KEY = 'test_a7fe6937b28c505f371ce8ca286'
+AUTH_TOKEN = 'test_a68ddfad2ea46762db39aadd1e3'
+api = Instamojo(api_key=API_KEY,
+                auth_token=AUTH_TOKEN,
+                endpoint='https://test.instamojo.com/api/1.1/')
+
+response = api.payment_request_create(
+	amount=25,
+	purpose="app test",
+	send_email=True,
+	email="tanayshah027@gmail.com",
+	redirect_url="https://amvmpayments-api.herokuapp.com/paymentrecieved",
+	)
+
 @app.route('/')
 def home():
     dict = { 'amt':15,'url':url}
     return render_template('homepage.html')
 
-@app.route('/paymentrecieved/',methods = ['GET'])
+@app.route('/paymentrecieved',methods = ['GET'])
 def accept_payment():
     id  = request.args.get('payment_id')
     stat = request.args.get('payment_status')
